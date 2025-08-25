791bac14b7558aa53c7dadbda210396efb64a0fa
Jeremy Harris <j1h7e7@gmail.com>
2025-08-24 23:28:48 -0700

    switch from gunicorn to granian (#11)

--- start_scripts/flask.sh
+++ start_scripts/flask.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 uv run --no-sync alembic upgrade head
-uv run --no-sync gunicorn main:flask_app --reload
+uv run --no-sync granian --interface wsgi main:flask_app --reload --host 0.0.0.0
